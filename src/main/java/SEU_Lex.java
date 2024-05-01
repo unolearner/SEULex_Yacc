4ffeb8bd9ccd78f55f06930d4c1e038fca201117
unolearner <1466615402@qq.com>
2024-05-01 15:39:04 +0800

    Initial commit

--- src/main/java/SEU_Lex.java
+++
@@ -1,6 +0,0 @@
-public class SEU_Lex {
-    public static void main(String[]args){
-        System.out.println("Hello World!");
-    }
-
-}
---
+++ src/main/java/SEU_Lex/Main.java
@@ -0,0 +1,7 @@
+package SEU_Lex;
+
+public class Main {
+    public static void main(String []args){
+        System.out.println("Hello world!");
+    }
+}
