738c1eea17e1d9ea1eb2f4531c0afdf4e06f31df
HankCaptain <127203740+HankCaptain@users.noreply.github.com>
2024-05-02 18:55:27 +0800

    Update Main.java
    文件读取初始化

--- src/main/java/SEU_Lex/Main.java
+++ src/main/java/SEU_Lex/Main.java
@@ -1,7 +1,15 @@
 package SEU_Lex;
 
 public class Main {
-    public static void main(String []args){
-
+        public static void main(String []args) throws IOException {
+        LFileParser yyl = new LFileParser();
+        yyl.initAll("littlec.l");
+        /*输出测试*/
+        /*
+        System.out.println(yyl.defineRules);
+        System.out.println(yyl.program1);
+        System.out.println(yyl.program2);
+        System.out.println(yyl.regexRules);
+        */
     }
 }
