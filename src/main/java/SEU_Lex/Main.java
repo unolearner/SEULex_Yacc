ef7495ff991d9cd209de327b68e3d0d5a2ef3733
unolearner <1466615402@qq.com>
2024-05-07 14:34:41 +0800

    finish reg filter, making it that ops in regs only contains +,|,*,(,)

--- src/main/java/SEU_Lex/Main.java
+++ src/main/java/SEU_Lex/Main.java
@@ -8,26 +8,33 @@ public class Main {
         LFileParser yyl = new LFileParser();
         yyl.initAll("littlec.l");
         /*输出测试*/
-        System.out.println(yyl.defineRules);
-        System.out.println("test");
-        System.out.println(yyl.program1);
-        System.out.println("test");
-        System.out.println(yyl.program2);
-        System.out.println("test");
+//        System.out.println(yyl.defineRules);
+//        System.out.println("test");
+//        System.out.println(yyl.program1);
+//        System.out.println("test");
+//        System.out.println(yyl.program2);
+//        System.out.println("test");
         System.out.println(yyl.regexRules);
 
         RegProcess.replaceBraces(yyl.defineRules);
         //System.out.println(yyl.defineRules);
         RegProcess.regs=yyl.defineRules;
+        //System.out.println(yyl.regexRules.size());
         for(var ety:yyl.regexRules){
                 String k=ety.getKey();
-                String tmp=k;
+                if(k.charAt(0)=='\"')   k=RegProcess.quoteErase(k);
+                k=RegProcess.turnToReal(k);
                 k=RegProcess.replaceBraces(k);
-                if(tmp.equals(k)){
-                        int idx=yyl.regexRules.indexOf(ety);
-                        yyl.regexRules.set(idx, new AbstractMap.SimpleEntry<>(k,ety.getValue()));
-                }
+                k=RegProcess.spChProcess(k);
+                k=RegProcess.turnBracketToOr(k);
+                //System.out.println(k);
+                int idx=yyl.regexRules.indexOf(ety);
+                yyl.regexRules.set(idx, new AbstractMap.SimpleEntry<>(k,ety.getValue()));
+
         }
-        //System.out.println(yyl.regexRules);
+        for(var ety:yyl.regexRules){
+                String test=ety.getKey();
+                System.out.println(test);
+        }
     }
 }
--- src/main/java/SEU_Lex/RegProcess.java
+++ src/main/java/SEU_Lex/RegProcess.java
@@ -2,6 +2,7 @@ package SEU_Lex;
 
 import java.util.Map;
 
+
 public class RegProcess {
     /*
     该类用于处理正规表达式，
@@ -11,6 +12,52 @@ public class RegProcess {
     字符全集：\t-~
      */
     public static Map<String,String> regs;
+    public static int ch_st=9,ch_ed=126;
+    public static String op="(|)?*.-^+\\";
+    public static String epsilon="\u0000";
+
+    public static String turnToReal(String src){
+        /*
+        将\n,\t,\r等变成其真正表示的字符，使两个字符变成一个
+         */
+        int srcLen = src.length();
+        String ans = "";
+        for (int i = 0; i < srcLen; ++i) {
+            char c = src.charAt(i);
+            if (c == '\\' && i + 1 < srcLen) {
+                ++i;
+                c = src.charAt(i);
+                switch (c) {
+                    case 'p':
+                        c = ' ';
+                        break;
+                    case 'r':
+                        c = '\r';
+                        break;
+                    case 'n':
+                        c = '\n';
+                        break;
+                    case 't':
+                        c = '\t';
+                        break;
+                    case 'v':
+                        c = '\u000b';
+                        break;
+                    case 'f':
+                        c = '\f';
+                        break;
+                    default:
+                        ans += '\\';
+                        break;
+                }
+                ans += c;
+                continue;
+            }
+            ans += c;
+        }
+        return ans;
+    }
+
     public static void replaceBraces(Map<String,String> reg){
         /*
         将分层定义的内容直接替换
@@ -42,17 +89,76 @@ public class RegProcess {
         /*
         将中括号中的字符用|连接
         [xX]——>x|X
+        [^]——>字符全集-^后面的
          */
-        return "";
+        int l=src.length(),rev=0,st=-1,ed=-1;
+        String tmp="",src_cp=src;
+        for(int i=0;i<l;i++){
+            char c=src.charAt(i);
+            if(c=='['){
+                if((i-1>=0&&src.charAt(i-1)=='\\'))  continue;
+                st=i;
+                if(src.charAt(i+1)=='^'){
+                    rev=1;
+                    i++;
+                }
+            }
+            else if(c==']') {
+                if((i-1>=0&&src.charAt(i-1)=='\\'))  continue;
+                ed=i;
+                if(rev==1){
+                    String s="";
+                    for(int j=ch_st;j<=ch_ed;j++){
+                        char ch=(char)j;
+                        if(!tmp.contains(String.valueOf(ch))) s+=ch;
+                    }
+                    tmp=s;
+                }
+                String ori=src.substring(st,ed+1);
+                int lt=tmp.length();
+                String t=tmp;
+                for(int j=0;j<lt-1;j++){
+                    char ch=tmp.charAt(j);
+                    t=t.replace(String.valueOf(ch),ch+"|");
+                }
+                t=t.replace(String.valueOf(tmp.charAt(lt-1)),""+tmp.charAt(lt-1));
+                tmp="";
+                src_cp=src_cp.replace(ori,'('+t+')');
+                st=ed=-1;
+            }
+            else{
+                if(c=='-'&&src.charAt(i-1)!='\\'){
+                    char pre=src.charAt(i-1);
+                    ++i;
+                    c=src.charAt(i);
+                    for(char j = (char) (pre+1); j<=c; j++){
+                        tmp+=j;
+                    }
+                }
+                else if(st==-1) continue;
+                else tmp+=c;//在中括号内的正常字符，直接加进去
+            }
+        }
+        return src_cp;
     }
 
     public static String quoteErase(String src){
         /*
-        将一些用引号包裹的正规式或关键字提取出来
+        将一些用引号包裹的正规式或关键字提取出来并在对其中的特殊字符作转义处理
         "auto"——>auto
          */
-
-        return "";
+        int l=src.length();
+        String str="";
+        for(int i=1;i<l-1;i++){
+            char ch=src.charAt(i);
+            if(op.contains(String.valueOf(ch))){
+                str+='\\';
+                str+=ch;
+            }else{
+                str+=ch;
+            }
+        }
+        return str;
     }
 
     public static String spChProcess(String src){
@@ -60,10 +166,51 @@ public class RegProcess {
         处理特殊字符：
         ?——>0个或一个前面的
         +——>一个前面的，然后*
-        .——>字符全集
-        [^]——>字符全集-^后面的所有字符
+        .——>字符全集,[\t-~]
          */
-
-        return "";
+        String src_copy=src;
+        int l=src.length(),lPar=-1,rPar=-1;
+        for(int i=0;i<l;i++){
+            char ch=src.charAt(i);
+            switch(ch){
+                case '?':
+                    if(i-1>=0&&src.charAt(i-1)=='\\')   continue;
+                    if(rPar==i-1){
+                        String tmp=src.substring(lPar,rPar+1);
+                        src_copy=src_copy.replace(tmp+'?','('+epsilon+'|'+tmp+')');
+                    }else{
+                        char c=src.charAt(i-1);
+                        src_copy=src_copy.replace(String.valueOf(c)+'?',"("+epsilon+'|'+c+')');
+                    }
+                    break;
+//                case '+':
+//                    if(i-1>=0&&src.charAt(i-1)=='\\')   continue;
+//                    if(rPar==i-1){
+//                        String tmp=src.substring(lPar,rPar+1);
+//                        src_copy=src_copy.replace(tmp+'+',tmp+tmp+'*');
+//                    }else{
+//                        char c=src.charAt(i-1);
+//                        src_copy=src_copy.replace(String.valueOf(c)+'+',String.valueOf(c)+c+'*');
+//                    }
+//                    break;
+                case '.':
+                    if(i-1>=0&&src.charAt(i-1)=='\\')   continue;
+                    src_copy=src_copy.replace(String.valueOf('.'),"[\t-~]");
+                    break;
+                case '(':
+                case '[':
+                    if(i-1>=0&&src.charAt(i-1)=='\\')   continue;
+                    lPar=i;
+                    break;
+                case ')':
+                case ']':
+                    if(i-1>=0&&src.charAt(i-1)=='\\')   continue;
+                    rPar=i;
+                    break;
+                default:
+                    break;
+            }
+        }
+        return src_copy;
     }
 }
